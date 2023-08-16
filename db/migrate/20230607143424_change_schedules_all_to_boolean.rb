1cd88a9383321a9894fc0512acddfa340861d746
azumino <ohnoasaki@OhnonoMacBook-Pro.local>
2023-08-16 18:52:55 +0900

    rails newコマンドを実行し、アプリケーションを作成

---
+++ db/migrate/20230607143424_change_schedules_all_to_boolean.rb
@@ -0,0 +1,9 @@
+class ChangeSchedulesAllToBoolean < ActiveRecord::Migration[6.1]
+  def up
+    change_column :schedules, :all, :boolean, using: "all::boolean"
+  end
+
+  def down
+    change_column :schedules, :all, :string
+  end
+end
---
+++ db/migrate/20230607173805_change_schedules_all_string.rb
@@ -0,0 +1,11 @@
+class ChangeSchedulesAllString < ActiveRecord::Migration[6.1]
+  def up
+    change_column :schedules, :all, :string
+  end
+
+  def down
+    change_column :schedules, :all, :boolean
+  end
+
+  
+end
